b0604b502b7cde731317f0a29c806a3c6901b58d
Brittany <bgreensl@ualberta.ca>
2021-04-11 22:00:40 -0600

    finished assignment and bonus

--- js/app2.js
+++ js/app2.js
@@ -1,28 +1,63 @@
-Cookies.get("token");
+//put cookies in variables to use later
+var token = Cookies.get("token");
 let userEmail = Cookies.get("email");
+
+//welcome message when user gets to page
 document.getElementById(
   "welcomeUser"
 ).innerHTML = `<h2>Welcome ${userEmail}!</h2>`;
 
+//logout button removes the cookies and sends user back to login  page
+function logout() {
+  if (token) {
+    Cookies.remove("token");
+    Cookies.remove("email");
+    window.open("./index.html", "_self");
+  }
+}
+//function called on page load, so anything I want to be loaded on page load is here
 function getColoursSuccess(res) {
+  document.getElementById(
+    "welcomeUser"
+  ).innerHTML += `<button id="logoutBtn" onclick="logout()">Logout</button>`;
   console.log(res);
   let colors = res.data.data;
+  //loops through colours data
   for (let i = 0; i < colors.length; i++) {
-    // let colorsContainer = document.getElementById("colorsContainer");
-    // colorsContainer.innerHTML += `<h3>${colors[i].name}</h3><h4>${colors[i].year}</h4>`;
+    //create a div, append it to the 'colorsContainer' so the names can be on top of the boxes
     let newDiv = document.createElement("div");
     colorsContainer.appendChild(
       newDiv
     ).innerHTML += `<h3>${colors[i].name}</h3><h4>${colors[i].year}</h4>`;
+    //add class so that I can add styling
     newDiv.classList.add("colourBox");
+    //make background of the new element the colour of the hex code given
     newDiv.style.background = `${colors[i].color}`;
   }
 }
+//error fn
 function getColoursError(err) {
   console.log(err);
 }
-
-function getColours(e) {
+//if user doesn't login
+function retryLogin() {
+  window.open("./index.html", "_self");
+}
+//if the login token cookie is undefined,
+if (token === undefined) {
+  //give user a little message n a button to go back and login
+  document.getElementById(
+    "welcomeUser"
+  ).innerHTML = `<h2>Sorry! It appears you're not logged in</h2>`;
+  document.getElementById(
+    "welcomeUser"
+  ).innerHTML += `<button id="retryLoginBtn">Login now</button>`;
+  let retryLoginBtn = document.getElementById("retryLoginBtn");
+  //call the 'retry login' function above that sends user back to homepage
+  retryLoginBtn.addEventListener("click", retryLogin);
+}
+//otherwise if the cookie is defined, send the request for the colours
+else if (token) {
   axios
     .request({
       method: "GET",
@@ -31,5 +66,3 @@ function getColours(e) {
     .then(getColoursSuccess)
     .catch(getColoursError);
 }
-
-window.addEventListener("load", getColours);
